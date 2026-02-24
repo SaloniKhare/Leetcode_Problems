da8535d3d506dc4697ae6cdb87ff9a7669e75820
SaloniKhare <salonikhare21@gmail.com>
2026-02-24 20:38:02 +0530

    Create Tabulation.py

---
+++ Matrix/Uniquie_Paths_II/Tabulation.py
@@ -0,0 +1,30 @@
+``` python
+class TabulationSolution:
+    def uniquePathsWithObstacles(self, obstacleGrid):
+        m = len(obstacleGrid)
+        n = len(obstacleGrid[0])
+        # If start is blocked
+        if obstacleGrid[0][0] == 1:
+            return 0
+        dp = [[0] * n for _ in range(m)]
+        dp[0][0] = 1
+        # First column
+        for i in range(1, m):
+            if obstacleGrid[i][0] == 0:
+                dp[i][0] = dp[i - 1][0]
+            else:
+                dp[i][0] = 0
+        # First row
+        for j in range(1, n):
+            if obstacleGrid[0][j] == 0:
+                dp[0][j] = dp[0][j - 1]
+            else:
+                dp[0][j] = 0
+        for i in range(1, m):
+            for j in range(1, n):
+                if obstacleGrid[i][j] == 0:
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+                else:
+                    dp[i][j] = 0
+        return dp[m - 1][n - 1]
+```
